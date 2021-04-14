c71c708a6f418aeefc793e53f420bd83cd7e9225
Trista Runions <t.runions19@gmail.com>
2021-04-13 20:34:41 -0700

    finished project

--- index.js
+++ index.js
@@ -39,11 +39,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name,age) {
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
+  }
+
+  Person.prototype.eat = function(someFood){
+    if (this.stomach.length < 10){
+      this.stomach.push(someFood);
+    }
   }
  
- 
+ Person.prototype.poop = function(){
+   this.stomach = [];
+ }
+
+ Person.prototype.toString = function(){
+   return `${this.name}, ${this.age}`
+ }
 
   
   
@@ -63,8 +77,17 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  Car.prototype.fill = function(gallons){
+    if (this.milesPerGallon >= gallons){
+      this.tank += gallons;
+    }
   }
   
   
@@ -75,18 +98,31 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
   }
- 
+
+
+ Baby.prototype = Object.create(Person.prototype);
+
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`
+  }
+
+
   
+
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Global/Window Binding: "this" is outside any object
+    2. Implicit Binding: "this" is inside an object
+    3. New Binding: "new" creats an object for "this" to bind to
+    4. Explicit Binding: uses .call/.apply to choose an object for "this" to bind with
   */
   
   
